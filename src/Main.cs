5db683e5217c486e77db6f9cd3b3be24db99c452
VladyslavZe <vladyslavze@tahometer.com>
2020-03-06 13:38:29 +0200

    Initial commit

---
+++ src/Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace src
+{
+  public class Main
+  {
+    static void main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
---
+++ test/DumbTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace test
+{
+  public class DumbTest
+  {
+    [Fact]
+    public void zero()
+    {
+      Assert.Equal(1, 1);
+    }
+  }
+}
